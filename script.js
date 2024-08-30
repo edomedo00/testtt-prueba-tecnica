49d9bf45bf5c791c530da89c99efab4b9bb324a3
edomedo <edramede@gmail.com>
2024-08-29 20:09:02 -0600

    everything is broken

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const openNavButton = document.getElementById('open-nav');
 const closeNavButton = document.getElementById('close-nav');
+const openNavButtonMobile = document.getElementById('open-nav-mobile');
+const closeNavButtonMobile = document.getElementById('close-nav-mobile');
 const nav = document.getElementById('nav-screen');
+const navMobile = document.getElementById('nav-screen-mobile');
 
 openNavButton.addEventListener('click', () => {
     nav.style.display = 'flex';
@@ -8,4 +11,12 @@ openNavButton.addEventListener('click', () => {
 
 closeNavButton.addEventListener('click', () => {
     nav.style.display = 'none';
+});
+
+openNavButtonMobile.addEventListener('click', () => {
+    navMobile.style.display = 'flex';
+});
+
+closeNavButtonMobile.addEventListener('click', () => {
+    navMobile.style.display = 'none';
 });
